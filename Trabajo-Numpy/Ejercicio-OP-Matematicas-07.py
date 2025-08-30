f6d3a76a6ab678e42518309f09fdccd99ca3506a
Bairon Diaz <baironft10@gmail.com>
2025-08-30 17:25:34 -0500

    Se agrega Ejercicio Mates

---
+++ Trabajo-Numpy/Ejercicio-OP-Matematicas-07.py
@@ -0,0 +1,18 @@
+import numpy as np
+"""
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+print("1. Operaciones matemáticas básicas")
+
+print("Suma: ", a + b)
+print("Multiplicación: ", a * b)
+"""
+
+
+notas = np.array([3.5, 4.0, 2.8, 5.0, 4.5])
+
+print("Promedio:", np.mean(notas))
+print("Mediana:", np.median(notas))
+print("Desviación estándar:", np.std(notas))
+print("Varianza:", np.var(notas))
